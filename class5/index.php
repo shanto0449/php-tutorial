1b194df24343ba648c8786bcd09d847e8345d4b4
shanto0449 <shantohossen22@gmail.com>
2024-09-03 05:35:06 -0700

    First!

---
+++ class5/index.php
@@ -0,0 +1,18 @@
+<?php 
+#single line coment
+// product price Calculation
+$first_price = 10; //Storing first price
+$second_price = 20;//Storing secend price
+$total_price= $first_price + $second_price; // Calculating total price
+echo $total_price .'<br>';
+
+#maltiline coment
+/*jdcndcndbdbnddmkjk
+xssjnxxnbdikdbdj
+sjnsxmddnxnmnnndcb
+ncndnccdjcdndndnd*/
+
+$result = 10 /*+5*/+10;
+echo $result;
+
+?>
