f5047f446decfb70463f959d3dd0ad7f4ee8fb4c
shanto0449 <shantohossen22@gmail.com>
2024-09-03 21:33:01 -0700

    same

---
+++ class7-constant/index.php
@@ -0,0 +1,13 @@
+<?php
+$name =" Raj ";
+$name="Shanto";
+echo $name .'<br>';
+?>
+
+<?php
+define("NAME","Raj");
+echo NAME .'<br>';
+$amount =20;
+echo "Your amount is: " .NAME ."<br>";
+echo constant ("NAME");
+?>
