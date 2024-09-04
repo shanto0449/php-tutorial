a42c232ffb6ce2fbfecdea9621a474b449e9e991
shanto0449 <shantohossen22@gmail.com>
2024-09-04 03:36:57 -0700

    fast

---
+++ class11-printf-2/index.php
@@ -0,0 +1,33 @@
+<?php
+$amunt=10.10;
+printf("Hello %f",$amunt );
+
+echo "<br>";
+
+$price=250;
+printf("proudct price %d", $price);
+
+echo "<br>";
+
+$name="Shanto";
+printf ("Hello %s", $name);
+
+echo "<br>";
+
+$test =10;
+$test2= 20;
+printf("Amount: %d Another Amount is: %d", $test, $test2);
+
+echo "<br>";
+
+printf("Amount: %2\$d Another Amount is: %d", $test, $test2);
+
+echo "<br>";
+
+printf('Amount: %2$d Another Amount is: %d', $test, $test2);
+
+echo "<br>";
+$nam="Shanto Hosen";
+printf("[%-'@12.5s]",$nam);
+
+?>
