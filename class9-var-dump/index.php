3d81b464eb53882294c8d7538a02abc66b4a961d
shanto0449 <shantohossen22@gmail.com>
2024-09-03 23:38:44 -0700

    fast

---
+++ class9-var-dump/index.php
@@ -0,0 +1,14 @@
+<?php
+$name ="Raj";
+var_dump($name );
+echo "<br>";
+$number= 20;
+var_dump($number);
+// echo $name;
+echo "<br>";
+$is_admin = true;
+var_dump($is_admin);
+//================
+echo "<br>";
+var_dump($name, $number, $is_admin);
+?>
