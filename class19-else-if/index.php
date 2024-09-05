4c4ce8eb5f8a7b5edfcfea78c548cb1cad524875
shanto0449 <shantohossen22@gmail.com>
2024-09-05 02:45:01 -0700

    fast

---
+++ class19-else-if/index.php
@@ -0,0 +1,12 @@
+<?php 
+$month_name = "ma";
+if( $month_name == 'jan' ){
+    echo "january";
+}else if( $month_name == 'feb' ) {
+    echo "February";
+}else if( $month_name == 'ma' ) {
+    echo "March";
+}else {
+    echo "Nothing";
+}
+?> 
