3e03e82bd92eb2cdfcd4b62657e65ab10391b5c0
shanto0449 <shantohossen22@gmail.com>
2024-09-04 22:15:33 -0700

    fast

---
+++ class15-concatination/index.php
@@ -0,0 +1,12 @@
+<?php
+$first_name = "Shanto";
+$last_name ="Hosen";
+$full_name = "Hello"." ".$first_name." ".$last_name;
+echo $full_name;
+
+echo "<br>";
+
+$first_name .=" Shawon";
+echo "$first_name";
+
+?>
