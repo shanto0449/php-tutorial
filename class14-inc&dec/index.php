dbe255898c1143c4c8f2afccb4b29aa3f2cb3683
shanto0449 <shantohossen22@gmail.com>
2024-09-04 21:50:23 -0700

    fast

---
+++ class14-inc&dec/index.php
@@ -0,0 +1,13 @@
+<?php
+ $amaunt = 10;
+  echo $amaunt++; //$amount = $amaunt+1 //$amount -=1
+ echo "<br>";
+  
+ echo $amaunt;
+ echo "<br>";
+ //Pre-increment
+  echo ++$amaunt;
+  echo "<br>";
+  echo $amaunt;
+
+?>
