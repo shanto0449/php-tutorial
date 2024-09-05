3608b1fa60eecda7d081a2e28a2d1584c792b102
shanto0449 <shantohossen22@gmail.com>
2024-09-05 00:41:04 -0700

    fast

---
+++ class18-if-else/index.php
@@ -0,0 +1,15 @@
+<?php
+$amount = 300;
+if( $amount > 250 ){
+    echo "Price is too high";
+}
+
+echo "<br>";
+
+$name = "abcd";
+if ( $name == 'abcd' ){
+    echo "Login Successfull";
+} else{
+    echo "login Failed";
+}
+?>
