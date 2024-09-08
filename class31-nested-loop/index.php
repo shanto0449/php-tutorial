553a07657a2ea72e1ddc205762d77631d4e40964
shanto0449 <shantohossen22@gmail.com>
2024-09-08 03:08:11 -0700

    first

---
+++ class31-nested-loop/index.php
@@ -0,0 +1,11 @@
+<?php
+for ($outer_list = 1; $outer_list <= 3; $outer_list++ ){
+    echo " list $outer_list <br> " ;
+    for($iner_list =1 ; $iner_list <= 2; $iner_list++){ 
+        echo "-----inner list $iner_list <br>";
+
+    }
+        
+    
+}
+?>
