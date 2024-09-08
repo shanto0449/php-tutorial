08cd1a04921a48219c8bea8347fe0d5c193dbb06
shanto0449 <shantohossen22@gmail.com>
2024-09-08 18:17:42 +0600

    fast

---
+++ class34-sprintf/index.php
@@ -0,0 +1,7 @@
+<?php 
+$first_name = "Shanto";
+$last_name = "Hosen";
+
+$new_str = sprintf("Your First Name is: %s and Your Last Name is: %s ", $first_name, $last_name);
+echo "new " . $new_str;
+?>
