151dbb71adb3ae6283a212e8754685831ee9285e
shanto0449 <shantohossen22@gmail.com>
2024-09-08 02:36:24 -0700

    fast

---
+++ class30-continue-statement/index.php
@@ -0,0 +1,11 @@
+<?php
+for ($count = 1 ; $count<= 10; $count++){
+    echo "PHP <br>";
+   if ( $count == 6){
+    continue;
+   }
+    echo "is <br>";
+    echo "awesome <br> <br><br>";
+   
+}
+?>
