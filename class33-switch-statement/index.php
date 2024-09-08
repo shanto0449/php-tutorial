8df4b943f7ff7bc52806fe2e5fbf1d5daf20284d
shanto0449 <shantohossen22@gmail.com>
2024-09-08 17:41:47 +0600

    fast

---
+++ class33-switch-statement/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = "Shanto";
+// if ($name == "Shanto" ){
+   
+//     echo "Hallo $name";
+// } else if ($name == "Rajnat"){
+//     echo "Hello $name";
+// } else {
+//     echo "By";
+// }
+
+switch ( $name){
+    case "raj";
+    echo "Hello $name";
+    case "Shanto";
+    echo "Hello $name";
+    break;
+    case "Rajesh";
+    echo "Hello $name";
+    break;
+    default:
+    echo "Byy";  
+}
+
+
+?>
